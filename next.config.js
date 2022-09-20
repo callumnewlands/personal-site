c6233ccb17c39ef0f58a048b9a5483c0da5d1e6f
Callum Newlands <callumnewlands@googlemail.com>
2022-09-20 21:42:57 +0100

    Fixes some mobile formatting bugs

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 const withAntdLess = require("next-plugin-antd-less");
 
-/**
- * @type {import("next").NextConfig}
- */
-const nextConfig = {
+// Override lessVarsFilePath with modifyVars
+module.exports = withAntdLess({
     modifyVars: { "@primary-color": "#8a1c10ff", "@border-radius-base": "6px" },
     lessVarsFilePath: "./styles/antd.less",
 
     webpack(config) {
         return config;
     }
-};
-
-// Override lessVarsFilePath with modifyVars
-module.exports = withAntdLess(nextConfig);
+});
