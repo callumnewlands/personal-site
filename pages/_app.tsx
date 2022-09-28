f811a6321cddfdb5a8cc4adb8f46493cd131b922
Callum Newlands <callumnewlands@googlemail.com>
2022-09-28 15:35:17 +0100

    Adds Google Analytics tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import Script from "next/script";
 
 function Application({ Component, pageProps }: AppProps) {
     return (
@@ -22,6 +23,17 @@ function Application({ Component, pageProps }: AppProps) {
                     href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@500;700;800&display=swap"
                 />
             </Head>
+            {/*<!-- Google tag (gtag.js) -->*/}
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9Y3PX0QM1E" strategy="afterInteractive" />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-9Y3PX0QM1E');
+                `}
+            </Script>
             <NavBar />
             <main>
                 <Component {...pageProps} />
